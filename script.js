0062727b0a7de34d64f67e70e073ae8f177f1b85
Anna R <anrd.rs@mail.ru>
2021-10-26 18:06:19 +0300

    eventListener and left column div added

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+let translitButton = document.getElementById('button');
+let userWord = document.getElementById('input');
+
+function translitUserWord(){
+  let userWordValue = userWord.value;
+  console.log(userWordValue);
+  let rusWord = document.createElement('div');
+  rusWord.innerText = userWordValue;
+  let rusWordBlock = document.getElementById('russian')
+  rusWordBlock.appendChild(rusWord)
+};
+
+translitButton.addEventListener('click', translitUserWord);
+
 
