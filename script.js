831f3b40eeaa73093bbb61a08e8da4b08420a62a
Anna R <anrd.rs@mail.ru>
2021-10-28 20:36:46 +0300

    final changes added

--- script.js
+++ script.js
@@ -7,12 +7,48 @@ function translitUserWord(){
   let rusWord = document.createElement('div');
   rusWord.innerText = userWordValue;
   rusWord.className = 'rusWordStyle';
-  rusWord.setAttribute('data-title', userWordValue)
-  let rusWordBlock = document.getElementById('russian')
-  rusWordBlock.appendChild(rusWord)
+  rusWord.setAttribute('data-title', userWordValue);
+  let rusWordBlock = document.getElementById('russian');
+  rusWordBlock.appendChild(rusWord);
+
+  let engWord = document.createElement('div');
+  engWord.innerText = translit(userWordValue);
+  engWord.className = 'engWordStyle';
+  engWord.setAttribute('data-title', translit(userWordValue))
+  let engWordBlock = document.getElementById('english')
+  engWordBlock.appendChild(engWord)
 };
 
 translitButton.addEventListener('click', translitUserWord);
 
-
+function translit(word){
+	let answer = '';
+	let converter = {
+		'а': 'a',    'б': 'b',    'в': 'v',    'г': 'g',    'д': 'd',
+		'е': 'e',    'ё': 'e',    'ж': 'zh',   'з': 'z',    'и': 'i',
+		'й': 'y',    'к': 'k',    'л': 'l',    'м': 'm',    'н': 'n',
+		'о': 'o',    'п': 'p',    'р': 'r',    'с': 's',    'т': 't',
+		'у': 'u',    'ф': 'f',    'х': 'h',    'ц': 'c',    'ч': 'ch',
+		'ш': 'sh',   'щ': 'sch',  'ь': '',     'ы': 'y',    'ъ': '',
+		'э': 'e',    'ю': 'yu',   'я': 'ya',
+ 
+		'А': 'A',    'Б': 'B',    'В': 'V',    'Г': 'G',    'Д': 'D',
+		'Е': 'E',    'Ё': 'E',    'Ж': 'Zh',   'З': 'Z',    'И': 'I',
+		'Й': 'Y',    'К': 'K',    'Л': 'L',    'М': 'M',    'Н': 'N',
+		'О': 'O',    'П': 'P',    'Р': 'R',    'С': 'S',    'Т': 'T',
+		'У': 'U',    'Ф': 'F',    'Х': 'H',    'Ц': 'C',    'Ч': 'Ch',
+		'Ш': 'Sh',   'Щ': 'Sch',  'Ь': '',     'Ы': 'Y',    'Ъ': '',
+		'Э': 'E',    'Ю': 'Yu',   'Я': 'Ya'
+	};  
+ 
+	for (let i = 0; i < word.length; ++i ) {
+		if (converter[word[i]] == undefined){
+			answer += word[i];
+		} else {
+			answer += converter[word[i]];
+		}
+	}
+ 
+	return answer;
+}
 
